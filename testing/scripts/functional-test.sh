5fac0906a97c34cd743a7f41929d214eaa1257c3
Peter Bengtsson <mail@peterbe.com>
2020-07-28 08:43:36 -0400

    debugging tests

--- testing/scripts/functional-test.sh
+++ testing/scripts/functional-test.sh
@@ -5,4 +5,8 @@ export ENV_FILE=testing/.env
 
 yarn build
 
+# Sanity check that certain things got built
+ls client/build/en-us/search-index.json
+ls client/build/en-us/docs/web/foo/index.html
+
 yarn workspace testing run test $@
